a33a9945b8c53ee6806c9bfe420c4cf394730f81
V. Shreyas <shreyasiitr20@gmail.com>
2017-03-25 15:20:30 +0530

    Works with keras 2.0

--- ConvNet.py
+++ ConvNet.py
@@ -16,12 +16,13 @@ from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras.layers import Dropout
+from keras.initializers import constant
 from keras import backend as K
 
 
 class LeNet:
     @staticmethod
-    def build_Pereira(w, h, d, classes, weightsPath = None, alp = 0.3, dropout = 0.1):
+    def build_Pereira(w, h, d, classes, weightsPath = None, alp = 0.333, dropout = 0.1):
         '''INPUT:
                 INPUT WIDTH, HEIGHT, DEPTH, NUMBER OF OUTPUT CLASSES, PRELOADED WEIGHTS, PARAMETER FOR LEAKYReLU, DROPOUT PROBABILITY
            OUTPUT:
@@ -32,20 +33,20 @@ class LeNet:
         
                 
         #first set of CONV => CONV => CONV => LReLU => MAXPOOL
-        model.add(Convolution2D(64, 3, 3, border_mode="same", input_shape = (d, h, w)))
+        model.add(Convolution2D(64, kernel_size=(3, 3), padding="same", data_format='channels_first', input_shape = (d, h, w), kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1) ))
         model.add(LeakyReLU(alpha=alp))
-        model.add(Convolution2D(64, 3, 3,border_mode="same", input_shape = (33, 33, 64)))
+        model.add(Convolution2D(64, kernel_size=(3, 3), border_mode="same", data_format='channels_first', input_shape = (64, 33, 33), kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1) ))
         model.add(LeakyReLU(alpha=alp))
-        model.add(Convolution2D(64, 3, 3, border_mode="same",input_shape = (33, 33, 64)))
+        model.add(Convolution2D(64, kernel_size=(3, 3), border_mode="same", data_format='channels_first', input_shape = (64, 33, 33), kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1) ))
         model.add(LeakyReLU(alpha=alp))
         model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
         
         #second set of CONV => CONV => CONV => LReLU => MAXPOOL
-        model.add(Convolution2D(128, 3, 3, border_mode="same",input_shape = (16, 16, 64)))
+        model.add(Convolution2D(128, kernel_size=(3, 3), border_mode="same", data_format='channels_first', input_shape = (64, 16, 16), kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1) ))
         model.add(LeakyReLU(alpha=alp))
-        model.add(Convolution2D(128, 3, 3, border_mode="same",input_shape = (16, 16, 128)))
+        model.add(Convolution2D(128, kernel_size=(3, 3), border_mode="same", data_format='channels_first', input_shape = (128, 16, 16), kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1) ))
         model.add(LeakyReLU(alpha=alp))
-        model.add(Convolution2D(128, 3, 3, border_mode="same",input_shape = (16, 16, 128)))
+        model.add(Convolution2D(128, kernel_size=(3, 3), border_mode="same", data_format='channels_first', input_shape = (128, 16, 16), kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1) ))
         model.add(LeakyReLU(alpha = alp))
         model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
         
@@ -53,15 +54,15 @@ class LeNet:
         
         # FC => LReLU => FC => LReLU
         model.add(Flatten())
-        model.add(Dense(256))
+        model.add(Dense(256, kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1)))
         model.add(LeakyReLU(alp))
         model.add(Dropout(dropout))
-        model.add(Dense(256))
+        model.add(Dense(256, kernel_initializer = 'glorot_normal', bias_initializer=constant(0.1)))
         model.add(LeakyReLU(alp))
         model.add(Dropout(dropout))
         
         # FC => SOFTMAX
-        model.add(Dense(classes))
+        model.add(Dense(classes, kernel_initializer = 'glorot_normal', bias_initializer = constant(0.1)))
         model.add(Activation("softmax"))
         
         #if a pre-trained model is applied, load the weights
